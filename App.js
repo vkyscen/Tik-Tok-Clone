97daaa98b542cd82a192bfa684a6051e45c65d1a
vkyscen <vickyeditz@gmail.com>
2020-10-13 02:47:15 +0530

    RecyclerListView fully implemented -> Status : untested

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { Dimensions, StyleSheet, Text, View} from 'react-native';
+import { Dimensions, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import Video from 'react-native-video';
-import {Container} from "native-base";
+import { RecyclerListView, DataProvider, LayoutProvider } from "recyclerlistview";
+import { Container } from "native-base";
 
-const {width , height} = Dimensions.get("window")
+const LocalArr = [
+  "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+  "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
+  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8"
+]
+
+const ViewTypes = {
+  FULL: 0,
+};
 export default function App() {
-  const [data, setData] = useState(null);
-  const arr =[
-    "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-    "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
-    "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-    "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8"
-  ]
+  const { width, height } = Dimensions.get("window")
+  const [videos, setVideos] = useState(null);
+  const [page, setPage] = useState(0);
+  const [dataProvider, setDataprovider] = useState(new DataProvider((r1, r2) => {
+    return r1 !== r2;
+  }));
+  const [layoutProvider, setLayoutprovider] = useState(new LayoutProvider(
+    index => ViewTypes.FULL,
+    (type, dim) => {
+      switch (type) {
+        case ViewTypes.FULL:
+          dim.width = width;
+          dim.height = height;
+          break;
+        default:
+          dim.width = 0;
+          dim.height = 0;
+      }
+    }
+  ));
+  inProgressNetworkReq = false
+
   useEffect(() => {
-    fetch("https://europe-west1-boom-dev-7ad08.cloudfunctions.net/videoFeed",{
+    fetch("https://europe-west1-boom-dev-7ad08.cloudfunctions.net/videoFeed", {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -24,8 +49,8 @@ export default function App() {
       })
     })
       .then((res) => res.json())
-      .then((json) => { 
-        setData(json)
+      .then((json) => {
+        setVideos(json)
         console.log(data)
       }
       )
@@ -33,25 +58,81 @@ export default function App() {
         console.log(e);
       });
   }, []);
+
+  fetchMoreData = async () => {
+    if (!inProgressNetworkReq) {
+      //To prevent redundant fetch requests. Needed because cases of quick up/down scroll can trigger onEndReached
+      //more than once
+      inProgressNetworkReq = true;
+      const videos = await DataCall.get(page);
+      inProgressNetworkReq = false;
+
+      setDataprovider(dataProvider.cloneWithRows(
+        [...videos, videos]
+      ),
+        setVideos([...videos, videos]),
+        // setPage(page + 1)
+      )
+    }
+  }
+
+  rowRenderer = (type, data) => {
+    switch (type) {
+      case ViewTypes.FULL:
+        return (
+          <Video
+            // source={{uri: "https://www.w3schools.com/html/mov_bbb.mp4"}}   
+            source={{ uri: "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8" }}
+
+            // onBuffer={this.onBuffer}                // Callback when remote video is buffering
+            // onError={this.videoError}  
+            resizeMode='stretch'             // Callback when video cannot be loaded
+            style={{ ...StyleSheet.absoluteFill }} />
+        );
+      default:
+        return null;
+    }
+  }
+
+  handleListEnd = () => {
+    setPage(page + 1)
+    fetchMoreData();
+
+    //This is necessary to ensure that activity indicator inside footer gets rendered. This is required given the implementation I have done in this sample
+    // this.setState({});
+  };
+
+  renderFooter = () => {
+    //Second view makes sure we don't unnecessarily change height of the list on this event. That might cause indicator to remain invisible
+    //The empty view can be removed once you've fetched all the data
+    return inProgressNetworkReq
+      ? <ActivityIndicator
+        style={{ margin: 10 }}
+        size="large"
+        color={'black'}
+      />
+      : <View style={{ height: 60 }} />;
+  };
   return (
     <View style={styles.container}>
-    <View style={{width: width , height: height,backgroundColor: "black"}}>
-    <Video 
-    // source={{uri: "https://www.w3schools.com/html/mov_bbb.mp4"}}   
-    source={{uri: "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8"}}   
-                                    
-    // onBuffer={this.onBuffer}                // Callback when remote video is buffering
-    // onError={this.videoError}  
-    resizeMode='stretch'             // Callback when video cannot be loaded
-    style={{...StyleSheet.absoluteFill}} />
-    </View> 
-    
+      {videos != 0
+        ? <RecyclerListView
+          style={{ flex: 1 }}
+          // contentContainerStyle={{ margin: 3 }}
+          onEndReached={handleListEnd}
+          dataProvider={dataProvider}
+          layoutProvider={layoutProvider}
+          rowRenderer={rowRenderer}
+          renderFooter={renderFooter}
+        />
+        : null}
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+
   backgroundVideo: {
     position: 'absolute',
     top: 0,
@@ -63,3 +144,20 @@ const styles = StyleSheet.create({
     flex: 1
   },
 });
+
+
+/*
+<View style={styles.container}>
+      <View style={{ width: width, height: height, backgroundColor: "black" }}>
+        <Video
+          // source={{uri: "https://www.w3schools.com/html/mov_bbb.mp4"}}
+          source={{ uri: "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8" }}
+
+          // onBuffer={this.onBuffer}                // Callback when remote video is buffering
+          // onError={this.videoError}
+          resizeMode='stretch'             // Callback when video cannot be loaded
+          style={{ ...StyleSheet.absoluteFill }} />
+      </View>
+
+    </View>
+*/

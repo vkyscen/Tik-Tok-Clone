16d18c76f5b1dcdbae282f9f4c2b1db1f6dc54af
vkyscen <vickyeditz@gmail.com>
2020-10-13 15:24:29 +0530

    recyclerListView completed with improved efficiency

--- App.js
+++ App.js
@@ -2,26 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { Dimensions, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import Video from 'react-native-video';
 import { RecyclerListView, DataProvider, LayoutProvider } from "recyclerlistview";
-import { DataCall } from './utils/DataCall';
+import { DataCall } from './Utils/DataCall';
 // import { Container } from "native-base";
-
-const LocalArr = [
-  "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-  "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
-  "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-  "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8"
-]
 
 const ViewTypes = {
   FULL: 0,
 };
 export default function App() {
   const { width, height } = Dimensions.get("window")
-  const [videos, setVideos] = useState(null);
+  const [videos, setVideos] = useState([]);
   const [page, setPage] = useState(0);
   const [dataProvider, setDataprovider] = useState(new DataProvider((r1, r2) => {
     return r1 !== r2;
-  }, (index) => videos[index].playbackUrl));
+  }
+    // , (index) => videos[index].playbackUrl
+  )
+  );
   const [layoutProvider, setLayoutprovider] = useState(new LayoutProvider(
     index => ViewTypes.FULL,
     (type, dim) => {
@@ -39,42 +35,27 @@ export default function App() {
   inProgressNetworkReq = false
 
   useEffect(() => {
-    fetchMoreData();
-    //   fetch("https://europe-west1-boom-dev-7ad08.cloudfunctions.net/videoFeed", {
-    //     method: 'POST',
-    //     headers: {
-    //       Accept: 'application/json',
-    //       'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //       page: 0,
-    //     })
-    //   })
-    //     .then((res) => res.json())
-    //     .then((json) => {
-    //       setVideos(json)
-    //       console.log(data)
-    //     }
-    //     )
-    //     .catch((e) => {
-    //       console.log(e);
-    //     });
-    // }, []);
-    fetchMoreData();
-  })
+    fetchMoreData()
+  }
+    , []);
+
+  useEffect(() => {
+    console.log(videos);
+  }, [videos]);
+
   fetchMoreData = async () => {
     if (!inProgressNetworkReq) {
-      //To prevent redundant fetch requests. Needed because cases of quick up/down scroll can trigger onEndReached
-      //more than once
+
       inProgressNetworkReq = true;
       const videos = await DataCall(page)
       inProgressNetworkReq = false;
 
       setDataprovider(dataProvider.cloneWithRows(
-        [...videos, videos]
+
+        videos.concat(videos)
       ))
-      // setPage(page + 1)
-      setVideos([...videos, videos])
+      setVideos(prevState => [...prevState, ...videos])
+
     }
   }
 
@@ -83,13 +64,14 @@ export default function App() {
       case ViewTypes.FULL:
         return (
           <Video
-            // source={{uri: "https://www.w3schools.com/html/mov_bbb.mp4"}}   
-            source={{ uri: "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8" }} //data.playbackUrl
-
-            // onBuffer={this.onBuffer}                // Callback when remote video is buffering
-            // onError={this.videoError}  
-            resizeMode='stretch'             // Callback when video cannot be loaded
-            style={{ ...StyleSheet.absoluteFill }} />
+            source={{ uri: data.playbackUrl }}
+            resizeMode='stretch'
+            style={{ ...StyleSheet.absoluteFill }}
+            selectedVideoTrack={{
+              type: "resolution",
+              value: 240
+            }}
+          />
         );
       default:
         return null;
@@ -100,8 +82,6 @@ export default function App() {
     setPage(page + 1)
     fetchMoreData();
 
-    //This is necessary to ensure that activity indicator inside footer gets rendered. This is required given the implementation I have done in this sample
-    // this.setState({});
   };
 
   renderFooter = () => {
@@ -120,12 +100,14 @@ export default function App() {
       {videos != null
         ? <RecyclerListView
           style={{ flex: 1 }}
-          // contentContainerStyle={{ margin: 3 }}
           onEndReached={handleListEnd}
           dataProvider={dataProvider}
           layoutProvider={layoutProvider}
           rowRenderer={rowRenderer}
           renderFooter={renderFooter}
+
+          renderAheadOffset={1.5}
+          onEndReachedThreshold={2}
         />
         : null}
 
--- Utils/DataCall.js
+++ Utils/DataCall.js
@@ -1,16 +1,19 @@
 
 // Just simulating incremental loading, don't infer anything from here
 export async function DataCall(pageNo) {
-    const responseVideo = await fetch("https://europe-west1-boom-dev-7ad08.cloudfunctions.net/videoFeed", {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            page: pageNo,
+    const responseVideo = await fetch(
+        // "https://europe-west1-boom-dev-7ad08.cloudfunctions.net/videoFeed", 
+        "http://192.168.1.6:5000/getVideos",
+        {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                page: pageNo,
+            })
         })
-    })
 
 
     const responseJsonVideos = await responseVideo.json();
